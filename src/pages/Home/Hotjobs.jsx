45714739100f476912286fdbe344ad1c9c6cf930
Jahid458 <Jahidul898@outlook.com>
2024-12-12 16:56:51 +0600

    hot jobs card

---
+++ src/pages/Home/Hotjobs.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import HotJobsCard from './HotJobsCard';
+
+const Hotjobs = () => {
+    const [jobs,setJobs] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/jobs')
+        .then(res => res.json())
+        .then(data => {
+            setJobs(data);
+        })
+
+    })
+    return (
+        <div>
+            <div className='grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+                {
+                    jobs.map(job => <HotJobsCard key={job._id} job={job}></HotJobsCard>)
+                }
+            </div>
+            
+        </div>
+    );
+};
+
+export default Hotjobs;
